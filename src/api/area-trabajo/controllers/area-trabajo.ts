e6b5b0f07d33efb36a911de7a9ce481ff30276ef
DiegoCP27 <dicornyt@gmail.com>
2023-04-13 12:29:58 -0500

    feat: table area-trabajo

---
+++ src/api/area-trabajo/controllers/area-trabajo.ts
@@ -0,0 +1,7 @@
+/**
+ * area-trabajo controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::area-trabajo.area-trabajo');
---
+++ src/api/area-trabajo/services/area-trabajo.ts
@@ -0,0 +1,7 @@
+/**
+ * area-trabajo service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::area-trabajo.area-trabajo');
